c380b803bb2be68499f98913a458126a8145de9d
exit <zrmartinezg@gmail.com>
2021-05-05 11:14:20 -0500

    Add devServer configuration in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,11 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  
+  devServer: {
+    contentBase: __dirname + '/dist',
+  },
+
   module: {
     rules: [
       {
